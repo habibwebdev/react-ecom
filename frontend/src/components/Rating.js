f25bfbff5a217a8b74679aadae26babc20bbbaa6
habibwebdev <abbasali211@gmail.com>
2020-12-07 13:32:35 +0500

    Fetching Products from API

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -75,10 +75,10 @@ Rating.defaultProps = {
   color: '#e8d924',
 }
 
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
-}
+// Rating.propTypes = {
+//   value: PropTypes.number.isRequired,
+//   text: PropTypes.string.isRequired,
+//   color: PropTypes.string,
+// }
 
 export default Rating
