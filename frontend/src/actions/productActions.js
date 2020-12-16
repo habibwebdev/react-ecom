9d7989e6393c0529547a294268b4095b38d0cfe9
habibwebdev <abbasali211@gmail.com>
2020-12-16 19:24:19 +0500

    HomeScreen with Redux State

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -12,7 +12,7 @@ export const listProducts = () => async (dispatch) => {
     const { data } = await axios.get('/api/products')
 
     dispatch({
-      type: PRODUCT_LIST_SUCCESSS,
+      type: PRODUCT_LIST_SUCCESS,
       payload: data,
     })
   } catch (error) {
