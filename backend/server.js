b47a23f44afb0eac939d673d28af5feb80333450
habibwebdev <abbasali211@gmail.com>
2020-12-22 18:00:06 +0500

    User Auth Endpoint completed

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -12,12 +13,16 @@ connectDB()
 
 const app = express()
 
+// allows json data body
+app.use(express.json())
+
 // Routes
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
