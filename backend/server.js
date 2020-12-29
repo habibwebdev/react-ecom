350cd359c6434742bb036d4c586a756e5928de16
habibwebdev <abbasali211@gmail.com>
2020-12-29 21:54:32 +0500

    Morgan installed & Config in Server file, User associated into productModel

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import path from 'path'
+import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
@@ -14,7 +15,13 @@ dotenv.config()
 
 connectDB()
 
+// express app
 const app = express()
+
+// middleware for morgan
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 // allows json data body
 app.use(express.json())
