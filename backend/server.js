6fc0a75aaeb90d9ef7586dc8a993da62b92de691
habibwebdev <abbasali211@gmail.com>
2020-12-29 13:49:39 +0500

    Image Upload Config & Endpoint completed

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import path from 'path'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -22,14 +24,21 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
+// Mount the Routes
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 // paypal config route
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+// mimicing the __dirname because its only available in es5 but not in es6 and we are using es6
+const __dirname = path.resolve()
+// making the uploads folder static
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
